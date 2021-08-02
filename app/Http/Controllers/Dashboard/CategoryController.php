e2bf99b658013dcde735a42b5d2488c8ed3ef09b
EC2 Default User <ec2-user@ip-172-31-47-221.us-west-2.compute.internal>
2021-08-02 08:28:45 +0000

    管理画面終了次１２章の退会機能から

---
+++ app/Http/Controllers/Dashboard/CategoryController.php
@@ -0,0 +1,98 @@
+<?php
+  
+  namespace App\Http\Controllers\Dashboard;
+  
+  use App\Category;
+  use App\MajorCategory;
+  use Illuminate\Http\Request;
+  use App\Http\Controllers\Controller;
+  
+  class CategoryController extends Controller
+  {
+      /**
+       * Display a listing of the resource.
+       *
+       * @return \Illuminate\Http\Response
+       */
+      public function index()
+      {
+          $categories = Category::paginate(15);
+
+         $major_categories = MajorCategory::all();
+
+         return view('dashboard.categories.index', compact('categories', 'major_categories'));
+      }
+  
+      /**
+       * Store a newly created resource in storage.
+       *
+       * @param  \Illuminate\Http\Request  $request
+       * @return \Illuminate\Http\Response
+       */
+      public function store(Request $request)
+      {
+          $category = new Category();
+          $category->name = $request->input('name');
+          $category->description = $request->input('description');
+         $category->major_category_id = $request->input('major_category_id');
+         $category->major_category_name = MajorCategory::find($request->input('major_category_id'))->name;
+          $category->save();
+  
+          return redirect("/dashboard/categories");
+      }
+  
+      /**
+       * Display the specified resource.
+       *
+       * @param  \App\Category  $category
+       * @return \Illuminate\Http\Response
+       */
+      public function show(Category $category)
+      {
+          //
+      }
+  
+      /**
+       * Show the form for editing the specified resource.
+       *
+       * @param  \App\Category  $category
+       * @return \Illuminate\Http\Response
+       */
+      public function edit(Category $category)
+      {
+         $major_categories = MajorCategory::all();
+
+         return view('dashboard.categories.edit', compact('category', 'major_categories'));
+      }
+  
+      /**
+       * Update the specified resource in storage.
+       *
+       * @param  \Illuminate\Http\Request  $request
+       * @param  \App\Category  $category
+       * @return \Illuminate\Http\Response
+       */
+      public function update(Request $request, Category $category)
+      {
+          $category->name = $request->input('name');
+          $category->description = $request->input('description');
+         $category->major_category_id = $request->input('major_category_id');
+         $category->major_category_name = MajorCategory::find($request->input('major_category_id'))->name;
+          $category->update();
+  
+          return redirect("/dashboard/categories");
+      }
+  
+      /**
+       * Remove the specified resource from storage.
+       *
+       * @param  \App\Category  $category
+       * @return \Illuminate\Http\Response
+       */
+      public function destroy(Category $category)
+      {
+          $category->delete();
+  
+          return redirect("/dashboard/categories");
+      }
+  }
--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,24 @@ Route::put('users/mypage/password', 'UserController@update_password')->name('myp
 Route::post('products/{product}/reviews','ReviewController@store');
 
 Route::get('products/{product}/favorite', 'ProductController@favorite')->name('products.favorite');
-Route::resource('products', 'ProductController');
+Route::get('products', 'ProductController@index')->name('products.index');
+Route::get('products/{product}', 'ProductController@show')->name('products.show');
 Auth::routes(['verify' => true]);
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/dashboard', 'DashboardController@index')->middleware('auth:admins');
+
+Route::group(['prefix' => 'dashboard', 'as' => 'dashboard.'], function () {
+    Route::get('login', 'Dashboard\Auth\LoginController@showLoginForm')->name('login');
+    Route::post('login', 'Dashboard\Auth\LoginController@login')->name('login');
+    Route::resource('major_categories', 'Dashboard\MajorCategoryController')->middleware('auth:admins');
+    Route::resource('categories', 'Dashboard\CategoryController')->middleware('auth:admins');
+    Route::resource('products', 'Dashboard\ProductController')->middleware('auth:admins');
+    Route::resource('users', 'Dashboard\UserController')->middleware('auth:admins');
+
+});
+
+if (env('APP_ENV') === 'production') {
+    URL::forceScheme('https');
+}
